b80d75fc9dc28fafd8a86ebd94e3b33c47689aba
Abel Ananta <abelananta9@gmail.com>
2023-06-16 19:51:21 +0700

    fix: delete console.log

--- src/lib/pokemon/index.js
+++ src/lib/pokemon/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { GET_POKEMON } from "../urlApi";
 
 export const getPokemon = async ({ limit, offset }) => {
-  console.log({ limit: limit, offset: offset });
   const res = await axios.get(GET_POKEMON, {
     params: {
       limit: limit,
